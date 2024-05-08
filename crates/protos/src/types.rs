206d0b83eb267703f2e97f50b65d76dc168cd9d9
Akira Moroo <retrage01@gmail.com>
2024-05-08 16:33:01 +0900

    build: Remove protos
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- crates/protos/src/types.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright (C) 2024 Akira Moroo
-// Copyright The containerd Authors.
-
-pub mod empty {
-    include!(concat!(env!("OUT_DIR"), "/types/empty.rs"));
-}
